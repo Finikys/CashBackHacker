23ec7a7e5bf5efc0b1d859888cb158c363d5c787
Finikys <ForOnceFinikys@gmail.com>
2023-02-25 13:28:46 +0300

    Second Chanсe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,3 @@
 public class Main {
-    public class CashbackHackService {
-        private final int boundary = 1000;
 
-        public int remain(int amount) {
-            return boundary - amount % boundary;
-        }
-    }
 }
